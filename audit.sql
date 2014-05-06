7eeee1341c608bd8fe434137a4f62294e3b94a00
gsieling <gsieling@wingspan.com>
2014-05-05 20:45:02 -0400

    update

--- audit.sql
+++ audit.sql
@@ -1,5 +1,122 @@
+o
+drop cense cascade;
 
-drop table license cascade;
+create table license (
+  id int primary key,
+  company text, 
+  effective_date tstzrange,
+  audit_user text,
+  audit_action text,
+  audit_date timestamp  
+);
+
+drop table movie cascade;
+
+create table movie (
+  id int primary key,
+  title text,
+  license_owner int references license (id) not null,
+  audit_user text,
+  audit_action text,
+  audit_date timestamp
+);
+
+create or replace view movies as
+select l.id license_id,
+       l.company license_company,
+       l.effective_date license_effective,
+       l.audit_user license_audit_user,
+       l.audit_date license_audit_range,
+       m.id movie_id,
+       m.title movie_title,
+       m.audit_user movie_audit_user,
+       m.audit_date movie_audit_date
+from movie m
+join license l on m.license_owner = l.id;
+ 
+
+CREATE RULE movie_insert AS ON INSERT
+TO movie
+DO NOTHING;
+
+CREATE RULE movie_delete AS ON DELETE
+TO movie
+DO NOTHING;
+
+CREATE RULE movie_update AS ON UPDATE
+TO movie
+DO NOTHING;
+
+
+CREATE RULE license_insert AS ON INSERT
+TO license
+DO NOTHING;
+
+CREATE RULE license_delete AS ON DELETE
+TO license
+DO NOTHING;
+
+CREATE RULE license_update AS ON UPDATE
+TO license
+DO NOTHING;
+
+drop sequence global_id;
+create sequence global_id;
+
+CREATE RULE movies_add AS ON INSERT
+TO movies
+DO INSTEAD (
+  INSERT 
+  INTO license
+    (id, company, effective_date, audit_user, audit_action, audit_date)
+    SELECT * FROM (  
+      SELECT
+        nextval('global_id'),
+        NEW.license_company,
+        NEW.license_effective,
+        'gary',
+        'I',
+        now()
+    ) a 
+    WHERE 
+      a.license_company NOT IN (select company FROM license);
+  
+  INSERT
+  INTO movie 
+     (id, title, license_owner, audit_user, audit_action, audit_date)
+  SELECT nextval('global_id'),
+         new.movie_title,
+         l.id,
+         'gary', 
+         'I',
+         now()
+   FROM license l
+   WHERE l.company = new.license_company
+   LIMIT 1;
+);
+
+select * from license;
+
+insert 
+into movies (movie_title, license_company, license_effective) 
+values ('Star Wars', 'Disney', '[2010-01-01 14:30, 2010-01-01 15:30)');
+
+select * from movies;
+/*
+INSERT INTO movies_audit VALUES (
+  new.id,
+  new.title,
+  new.license_owner,
+  new.license_effective,
+  current_user,
+  'U',
+  now()  
+);
+
+insert into movies_audit
+(id, title, license_owner, license_effective) 
+values (1, 'Star Wars', 'Disney', tstzrange(now(), now()))
+*/
 
 create table license (
   id int primary key,
@@ -109,4 +226,4 @@ INSERT INTO movies_audit VALUES (
 insert into movies_audit
 (id, title, license_owner, license_effective) 
 values (1, 'Star Wars', 'Disney', tstzrange(now(), now()))
-*/
+*
