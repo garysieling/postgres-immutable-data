231552704a36409895f596f91018018b67f0b2cc
Gary Sieling <gary@garysieling.com>
2014-08-31 20:56:21 -0400

    Add blame query

--- db.sql
+++ db.sql
@@ -226,13 +226,35 @@ where l.movie_id = m.id;
 -- Test queries
 
 -- Blame query
-SELECT * FROM (
-  SELECT *
-  FROM movies$a
-  WHERE id = ...
-  ORDER BY audit_date DESC
-) LIMIT 1
-
+select 
+  *
+from 
+(
+  select 
+    audit_date,
+    audit_user,
+    audit_action,
+    id,
+    title,
+    (case when ne(title, lead(title) over y) then audit_user else null end) title$u, 
+    dense_rank() over y title$r,
+    title$c
+  from (
+    select 
+      id,
+      title,
+      ne(title, lead(title) over w) title$c,
+      audit_date,
+      audit_user,
+      audit_action
+    from movies$a
+    where audit_action in ('I', 'U')
+    WINDOW w AS ( PARTITION by id ORDER BY audit_date DESC )
+  ) a
+  WINDOW y AS ( PARTITION by id ORDER BY audit_date DESC, title$c DESC )
+) b
+WHERE title$r = 1
+;
 
 -- Range test
 select
